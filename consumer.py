aa7002969416059e8bb411eed9a69673aa05bf40
Karthik J Ghorpade <Karthikjagadish5@gmail.com>
2024-01-02 15:28:12 +0530

    Adding comments to consumer.py

--- consumer.py
+++ consumer.py
@@ -1,10 +1,15 @@
 from kafka import KafkaConsumer
+
+# Creating a KafkaConsumer subscribing to the topic bank_branch
 consumer = KafkaConsumer('bank_branch',
                         group_id=None,
                          bootstrap_servers=['localhost:9092'],
                          auto_offset_reset = 'earliest')
+
 print("Hello")
 print(consumer)
 
+# Once the consumer is created, it will receive all available messages from the topic bank_branch. 
+# Then we can iterate and print them with the following code snippet
 for msg in consumer:
     print(msg.value.decode("utf-8"))
